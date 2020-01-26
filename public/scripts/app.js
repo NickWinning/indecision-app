cbc57f98f230fdf921e2fd31f46f3513050e41b4
Nicholas Nguyen <nnguyen@barracuda.com>
2020-01-26 15:59:30 -0800

    Updating code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,88 @@
-"use strict";
+'use strict';
 
-console.log("App.js is running");
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "This is JSX from App.js"
-);
-var appRoot = document.getElementById('app');
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
 
-ReactDOM.render(template, appRoot);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = ' ';
+        renderAddOption();
+    }
+};
+
+var removeAll = function removeAll() {
+    app.options = [];
+    renderAddOption();
+};
+
+var renderAddOption = function renderAddOption() {
+    var template = React.createElement(
+        'div',
+        { onSubmit: onFormSubmit },
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item One'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item Two'
+            )
+        ),
+        React.createElement(
+            'form',
+            null,
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+var appRoot = document.getElementById('app');
+renderAddOption();
--- src/app.js
+++ src/app.js
@@ -1,7 +1,50 @@
-console.log("App.js is running");
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <h1>This is JSX from App.js</h1>;
-var appRoot = document.getElementById('app');
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
 
-ReactDOM.render(template, appRoot)
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = ' ';
+        renderAddOption();
+    }
+};
+
+const removeAll = () => {
+    app.options = [];
+    renderAddOption();
+};
+
+const renderAddOption = () => {
+    const template = (
+        <div onSubmit={onFormSubmit}>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={removeAll}>Remove All</button>
+            <ol>
+                <li>Item One</li>
+                <li>Item Two</li>
+            </ol>
+            <form>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+const appRoot = document.getElementById('app');
+renderAddOption();
